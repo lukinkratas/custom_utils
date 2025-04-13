d040a6e8e2e9bc7e719331b58020b29598b58b32
Lukin Kratas <kratochvila.lukin@gmail.com>
2025-04-13 21:40:05 +0200

    update unit tests

--- tests/test_dataframe_utils.py
+++ tests/test_dataframe_utils.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
-import csv_utils
+import dataframe_utils
 
 default_df: pd.DataFrame = pd.DataFrame({'name': ['Adam', 'Eva'], 'userid': [10, 11]})
 default_bytes: bytes = b'name,userid\nAdam,10\nEva,11\n'
 
 
 def test_decode_csv() -> None:
-    assert default_df.equals(csv_utils.decode_to_df(default_bytes))
+    assert default_df.equals(dataframe_utils.decode_to_df(default_bytes))
 
 
 def test_encode_csv() -> None:
-    assert csv_utils.encode_df(default_df) == default_bytes
+    assert dataframe_utils.encode_df(default_df) == default_bytes
